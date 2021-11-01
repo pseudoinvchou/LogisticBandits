b9d0edc999adbb28bf636cc5407893f1043c6c18
Shuai Liu <1170500804hit@gmail.com>
2021-10-31 23:58:18 -0600

    linear_regrets

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# LogisticBandits" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/pseudoinvugly/LogisticBandits.git
+git push -u origin main
